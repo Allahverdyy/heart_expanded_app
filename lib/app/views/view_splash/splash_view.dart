3a6c3b435145367446c87160f196374effad19c8
ABDULSAMED <samet.1198@gmail.com>
2023-10-16 15:02:28 +0300

    add ✨: some files add

---
+++ lib/app/views/view_splash/splash_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:auto_route/auto_route.dart';
+
+@RoutePage()
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height,
+            child: Image.asset(
+                                'assets/images/png/images_splash_bg.png', fit: BoxFit.fill,),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                                'assets/images/png/images_splash_logo.png'),
+              ),
+              const Text(
+                'Heart Expanded',
+                style: TextStyle(color: Colors.pink, fontSize: 20),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+  
+}
+
