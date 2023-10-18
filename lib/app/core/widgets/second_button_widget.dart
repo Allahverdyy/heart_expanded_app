82846b5c871dac868b5b57144719b7ab6281fd00
ABDULSAMED <samet.1198@gmail.com>
2023-10-18 16:07:41 +0300

    add ✨: second button add

---
+++ lib/app/core/widgets/second_button_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SecondButtonWidget extends StatelessWidget {
+  final List<Color> secondButtonBg;
+  final Color imageColor;
+  const SecondButtonWidget({
+    super.key,
+    this.secondButtonBg = const <Color>[
+            Colors.black,
+            Colors.black
+          ],
+    this.imageColor = Colors.purple
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 9 / 10,
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: const Alignment(0.8, 1),
+          colors:secondButtonBg,
+          tileMode: TileMode.mirror,
+        ),
+      ),
+      child: TextButton(
+        onPressed: () {},
+        child: Icon(Icons.heart_broken_sharp,
+            color: imageColor,
+            size: MediaQuery.sizeOf(context).width / 8),
+      ),
+    ));
+  }
+}
