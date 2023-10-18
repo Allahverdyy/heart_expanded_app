0d4a604a6482e76691125cec71408fe9786c9474
ABDULSAMED <samet.1198@gmail.com>
2023-10-18 15:59:01 +0300

    add ✨: first button widget add

---
+++ lib/app/core/widgets/first_button_widget.dart
@@ -0,0 +1,45 @@
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:heart_expanded_app/app/core/constants/images_constants.dart';
+import 'package:heart_expanded_app/app/router/app_router.gr.dart';
+
+class FirstButtonWidget extends StatelessWidget {
+  final List<Color> firstButtonBg;
+  final Color imageColor;
+  const FirstButtonWidget({
+    super.key,
+    this.firstButtonBg = const <Color>[
+            Colors.black,
+            Colors.black
+          ],
+    this.imageColor = Colors.purple
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 9 / 10,
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: const Alignment(0.8, 1),
+          colors:firstButtonBg,
+          tileMode: TileMode.mirror,
+        ),
+      ),
+      child: TextButton(
+        onPressed: () {
+          context.router.push(const HeartScreenOneRoute());
+        },
+        child: Image.asset(
+          flutterHeart,
+          width: MediaQuery.of(context).size.width / 8,
+          color: imageColor
+        ),
+      ),
+    ));
+  }
+}
